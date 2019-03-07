34590d7e1dc03643c6f57492f027df29bd89bc2f
Jack Lam <jlam00@stuy.edu>
2019-03-07 11:46:50 -0500

    tried

--- USACO.java
+++ USACO.java
@@ -1,17 +1,20 @@
 import java.util.*;
 import java.io.*;
 public class USACO{
+  private static int[][] field;
+  private static int elevation;
+  private static int moves;
   public static int bronze(String filename){
-    int elevation;
-    int moves;
+    elevation = -1;
+    moves = -1;
     int lines = 1;
     String[] data;
-    int[][] field = new int [0][0];
      try{
        File text = new File(filename);
        Scanner stuff = new Scanner(text);
        while(stuff.hasNextLine()){
          String line = stuff.nextLine();
+
          data = line.split(" ");
          if (lines == 1){
          field = new int[Integer.parseInt(data[0])][Integer.parseInt(data[1])];
@@ -22,27 +25,50 @@ public class USACO{
          for (int c = 0; c < data.length; c++){
            field[lines-2][c] = Integer.parseInt(data[c]);
          }
-        System.out.println(field);
+       }
+       if (lines >= field.length +2){
+         int biggest = 0;
+         for(int cowr = Integer.parseInt(data[0]) -1; cowr <Integer.parseInt(data[0]) +2; cowr++){
+           for(int cowc = Integer.parseInt(data[1]) -1; cowc <Integer.parseInt(data[1]) +2; cowc++){
+             System.out.println(biggest + "\n" + field[cowr][cowc] + "\n");
+            System.out.println(cowr + " "+ cowc);
+             if (field[cowr][cowc] > biggest){
+               biggest = field[cowr][cowc];
+             }
+           }
+         }
+        biggest -= Integer.parseInt(data[2]);
+        for(int cowr = Integer.parseInt(data[0]) -1; cowr <Integer.parseInt(data[0]+ 2); cowr++){
+          for(int cowc = Integer.parseInt(data[1]) -1; cowr <Integer.parseInt(data[1 +2]); cowc++){
+            if (field[cowr][cowc] > biggest){
+              field[cowr][cowc] = biggest;
+            }
+          }
+        }
        }
        lines += 1;
+
+      System.out.println(line);
        }
+
      }
      catch(FileNotFoundException e){
        System.out.println("errrror");
      }
-    // System.out.println(field);
-     return -1;
+    System.out.println("\n");
+     String ans = "";
+     for (int r =0; r < field.length; r++){
+       for (int c =0; c <field[r].length; c++){
+         ans += field[r][c] + " ";
+       }
+       ans += "\n";
+     }
+     //System.out.println(elevation + " " + moves);
+     System.out.println(ans);
+
+   return -1;
   }
-  public String toString(){
-    String ans = "";
-    for (int r =0; r < field.length; r++){
-      for (int c =0; c <field[r].length; c++){
-        ans += field[r][c];
-      }
-      ans += "\n";
-    }
-    return ans;
-  }
+
   public static void main(String[] args) {
     bronze("bronze.txt");
   }
